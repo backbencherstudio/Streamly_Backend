d50c8acf6a2fc62fe17194de9bf1e792a32fa28c
Sazedul Islam <sazedulislam9126@gmail.com>
2025-08-10 09:14:45 +0600

    google loging

--- modules/user/user.route.js
+++ modules/user/user.route.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   registerUser,
   loginUser,
@@ -9,53 +9,54 @@ import {
   updateUserDetails,
   changePassword,
   sendMailToAdmin,
-  getMe
-} from './user.controller.js';
-import { upload } from '../../config/Multer.config.js';
-import { verifyUser } from '../../middlewares/verifyUsers.js';
-
+  getMe,
+  googleLogin,
+} from "./user.controller.js";
+import { upload } from "../../config/Multer.config.js";
+import { verifyUser } from "../../middlewares/verifyUsers.js";
 
 const router = express.Router();
 // Test route
-router.get('/test', (req, res) => {
-  res.send('User route connected');
+router.get("/test", (req, res) => {
+  res.send("User route connected");
 });
-
 
 // File upload route
-router.post('/upload', upload.single('file'), (req, res) => {
+router.post("/upload", upload.single("file"), (req, res) => {
   if (!req.file) {
-    return res.status(400).send('No file uploaded');
+    return res.status(400).send("No file uploaded");
   }
-  res.status(200).send({ message: 'File uploaded successfully', file: req.file });
+  res
+    .status(200)
+    .send({ message: "File uploaded successfully", file: req.file });
 });
 
-
 //Register a user
-router.post('/registerUser', registerUser);
+router.post("/registerUser", registerUser);
 //log in a user
-router.post('/login', loginUser);
+router.get("/login", loginUser);
 
-
+// Google login
+router.patch("/google-login", googleLogin);
 
 //forget pass
-router.post('/forget_pass', forgotPasswordOTPsend);
-router.post('/checkForgetPassOtp', verifyForgotPasswordOTP);
-router.post('/resetPass', resetPassword);
-router.post('/change-password', verifyUser("USER"), changePassword);
-
-
+router.post("/forget_pass", forgotPasswordOTPsend);
+router.post("/checkForgetPassOtp", verifyForgotPasswordOTP);
+router.post("/resetPass", resetPassword);
+router.post("/change-password", verifyUser("USER"), changePassword);
 
 //update user img
-router.put('/update-image', upload.single('profilePicture'), verifyUser("USER"), updateImage);
-router.put('/update-user-details', verifyUser("USER"), updateUserDetails);
-
-
+router.put(
+  "/update-image",
+  upload.single("profilePicture"),
+  verifyUser("USER"),
+  updateImage
+);
+router.put("/update-user-details", verifyUser("USER"), updateUserDetails);
 
 //support
-router.post('/sende-mail', verifyUser("USER"), sendMailToAdmin)
+router.post("/sende-mail", verifyUser("USER"), sendMailToAdmin);
 
-
-//get me 
-router.get('/get-me', verifyUser("USER"), getMe);
+//get me
+router.get("/get-me", verifyUser("USER"), getMe);
 export default router;
