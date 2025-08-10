e6d1ebc18fd07f0d91a53f597e6e6717d942ead9
Sazedul Islam <sazedulislam9126@gmail.com>
2025-08-10 08:41:58 +0600

    updated

--- modules/admin/users/users.route.js
+++ modules/admin/users/users.route.js
@@ -4,9 +4,10 @@ import { randomUUID } from 'crypto';
 import { PrismaClient } from '@prisma/client';
 import { getAllUsers } from './users.controller.js';
 import { get } from 'http';
+import { verifyUser } from '../../../middlewares/verifyUsers.js';
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.get('/allusers', getAllUsers);
+router.get('/allusers',  verifyUser("admin"), getAllUsers);
 export default router;
