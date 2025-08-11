8ad5edc29cdeac1f36b7ad7ac56c2cdf52732d84
Sazedul Islam <sazedulislam9126@gmail.com>
2025-08-11 10:57:55 +0600

    added suspended user

--- modules/admin/users/users.route.js
+++ modules/admin/users/users.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import { randomUUID } from 'crypto';
 import { PrismaClient } from '@prisma/client';
-import { getAllUsers } from './users.controller.js';
+import { getAllUsers,suspendUser } from './users.controller.js';
 import { get } from 'http';
 import { verifyUser } from '../../../middlewares/verifyUsers.js';
 
@@ -10,4 +10,5 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.get('/allusers',  verifyUser("admin"), getAllUsers);
+router.post('/suspenduser/:id', verifyUser("admin"), suspendUser);
 export default router;
