79c450ece4cee2cb0bfc9d23d44ebabe35ddf3b4
Sazedul Islam <sazedulislam9126@gmail.com>
2025-08-17 12:23:18 +0600

    Update schema, Added SupportTickets features.

---
+++ modules/helpSupport/support.controller.js
@@ -0,0 +1,68 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+//---------------------create support ticket-------------------
+export const createSupportTicket = async (req, res) => {
+  try {
+    const user = req.user;
+
+    const { subject, description } = req.body;
+
+    if (!subject || !description) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const newTicket = await prisma.helpSupport.create({
+      data: {
+        user_id: user?.userId,
+        subject,
+        description,
+      },
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Support ticket created successfully",
+      ticket: newTicket,
+    });
+  } catch (error) {
+    console.error("Error in createSupportTicket:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+//-------------------get all tickets-------------------
+export const getAllTickets = async (req, res) => {
+  try {
+    const tickets = await prisma.helpSupport.findMany();
+    return res.status(200).json({
+      success: true,
+      tickets,
+    });
+  } catch (error) {
+    console.error("Error in getAllTickets:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+//-------------------solve ticket-------------------
+export const solveTicket = async (req, res) => {
+  try {
+    const { ticketId } = req.params;
+
+    const updatedTicket = await prisma.helpSupport.update({
+      where: { id: ticketId },
+      data: { status: "Resolved" },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Ticket resolved successfully",
+      ticket: updatedTicket,
+    });
+  } catch (error) {
+    console.error("Error in solveTicket:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
