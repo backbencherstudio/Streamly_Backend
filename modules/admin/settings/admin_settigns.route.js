bbd23c1cb97f38baf869f841d075ad03d7792950
Sazedul Islam <sazedulislam9126@gmail.com>
2025-08-17 10:13:54 +0600

    Block the user if deactivated & activate user

--- modules/admin/settings/admin_settigns.route.js
+++ modules/admin/settings/admin_settigns.route.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { verifyAdmin } from "../../../middlewares/verifyAdmin.js";
 import {
+  activateUser,
   deactivateAccount,
   deleteAccount,
 } from "./admin_settings.controller.js";
+import { verifyUser } from "../../../middlewares/verifyUsers.js";
 
 const router = express.Router();
 
-router.post("/deactivate/:userId", verifyAdmin, deactivateAccount);
-router.delete("/delete/:userId", verifyAdmin, deleteAccount);
+router.post("/deactivate/:userId", verifyAdmin, verifyUser, deactivateAccount);
+router.post("/activate/:userId", verifyAdmin, verifyUser, activateUser);
+router.delete("/delete/:userId", verifyAdmin, verifyUser, deleteAccount);
 
 export default router;
